32f6b3eb2ba5518361d885438c8bb72dd8a24c31
pabloojdr <pablojulian.campoyfernadez@studenti.unipd.it>
2024-09-02 16:10:02 +0200

    feat: :sparkles: code client first version

---
+++ client.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 15:36:05 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/02 15:36:05 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include "./ft_printf/ft_printf.h"
+#include "./libft/libft.h"
+
+void	send_signal(pid_t server_pid, char c)
+{
+	int bits;
+
+	bits = 7;
+	while (bits >= 0)
+	{
+		if ((c >> bits) & 1)
+			kill(server_pid, SIGUSR2);
+		else
+			kill(server_pid, SIGUSR1);
+		usleep(100000);
+		bits--;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	pid_t	serverpid;
+	char	*str;
+	int		cnt;
+
+	if (argc != 3)
+	{
+		printf("Invalid number of arguments\n");
+		return (0);
+	}
+	serverpid = ft_atoi(argv[1]);
+	str = argv[2];
+	cnt = 0;
+	while (str[cnt] != 0)
+		send_signal(serverpid, str[cnt++]);
+	return (0);
+}
