b7a7a190d5cb7d78c3efd311cc7d991df3791b87
pabloojdr <pabloojdr@gmail.com>
2024-09-02 17:31:54 +0200

    feat: :sparkles: add ft_printf funcitions and libraries

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pcampoy- <pcampoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:59:25 by pcampoy-          #+#    #+#             */
-/*   Updated: 2024/04/25 14:11:12 by pcampoy-         ###   ########.fr       */
+/*   Updated: 2024/09/02 17:24:06 by pcampoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
 typedef struct s_list
 {
@@ -342,5 +343,53 @@ void	ft_lstiter(t_list *lst, void (*f) (void *));
 /// de un nodo si hace falta
 /// @return La nueva lista. NULL si falla la memoria
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+/// @brief Prints a character.
+/// @param c Character to print
+/// @return 1
+int		ft_putchar(char c);
+
+/// @brief Prints a string of characters
+/// @param str String to print
+/// @return Number of characters printed
+int		ft_putstr(char *str);
+
+/// @brief Prints a decimal number converted to an hexadecimal one
+/// @param n Decimal number to print
+/// @param c Character used to know wether an uppercase or lowercase 
+/// alphabet is required
+/// @return Number of characters printed 
+int		ft_hexa(unsigned long int n, char c);
+
+/// @brief Counts the number of digits in a number (including the sign)
+/// @param n 
+/// @return number of digits
+int		digits(int n);
+
+/// @brief Prints a decimal number in base 10
+/// @param n Decimal number to print
+/// @return Number of characters printed
+int		ft_putnumber(int n);
+
+/// @brief Counts the number of digits in an unsigned number
+/// @param n 
+/// @return Number of digits
+int		digits_unsigned(unsigned int n);
+
+/// @brief Prints an unsigned number in base 10
+/// @param n Unsigned number to print
+/// @return Number of digits printed
+int		ft_putnumber_unsigned(unsigned int n);
+
+/// @brief Prints a pointer direction
+/// @param n Pointer direction
+/// @return Number of characters written
+int		ft_putvoid(unsigned long int n);
+
+/// @brief Printf function
+/// @param s 
+/// @param  
+/// @return 
+int		ft_printf(char const *s, ...);
 
 #endif
