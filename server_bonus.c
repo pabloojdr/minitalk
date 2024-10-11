c3843af2c4f3ec3e768d8660b3311a0e762ad48b
pabloojdr <pabloojdr@gmail.com>
2024-10-11 23:16:18 +0200

    fix: :bug: fix some errors in sigaction structure and sig confirmation

--- server_bonus.c
+++ server_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pabloojdr <pabloojdr@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:19:17 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/11 22:19:17 by marvin           ###   ########.fr       */
+/*   Updated: 2024/10/11 23:02:34 by pabloojdr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
 	static char	currentchar = 0;
 	static char	bitcount = 0;
-    (void)  context;
 
+	(void) ucontext;
 	currentchar <<= 1;
-	if (signo == SIGUSR2)
+	if (sig == SIGUSR2)
 		currentchar |= 1;
 	bitcount++;
 	if (bitcount == 8)
@@ -29,7 +29,10 @@ void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 		currentchar = 0;
 		bitcount = 0;
 	}
-    kill(info->si_pid,SIGUSR1);
+	if (sig == SIGUSR1)
+    	kill(info->si_pid,SIGUSR1);
+	else if (sig == SIGUSR2)
+		kill(info->si_pid, SIGUSR2);
 }
 
 int	main(void)
@@ -40,9 +43,10 @@ int	main(void)
 	pid = getpid();
 	ft_printf("Server PID: %i\n", pid);
 	sa.sa_sigaction = sig_handler;
-    sa.sa_flags = SA_SIGINFO;
+    sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
     sigaction(SIGUSR1, &sa, 0);
-    sigaction(SIGUSR, &sa, 0);
+    sigaction(SIGUSR2, &sa, 0);
 	while (1)
 	{
 		pause();
